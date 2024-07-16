db5c2b18eb318211158d54051099cadb86766d9b
alvin <alvin@mabolotech.com>
2024-07-16 12:47:18 +0800

    better nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,57 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @next/next/no-img-element */
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { CiMenuKebab } from "react-icons/ci";
 import { IoCloseSharp } from "react-icons/io5";
 import { FaFacebookF } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa6";
 import { FaTwitter } from "react-icons/fa";
-import Home from "./pages/home";
 import Recipe from "./pages/recipe";
-import Blog from "./pages/blog";
+import Contact from "./pages/contact";
 
 export default function Nav() {
-  const [menuBtn, setMenuBtn] = React.useState(false);
+  const [menuBtn, setMenuBtn] = useState(false);
+  const [prevScrollPos, setPrevScrollPos] = useState(window.pageYOffset);
+  const [visible, setVisible] = useState(true);
+  const [menuClicked, setMenuClicked] = useState(false);
 
-  const menuVisibilty = () => {
-    setMenuBtn(true);
+  const handleScroll = () => {
+    if (!menuClicked) {
+      const currentScrollPos = window.pageYOffset;
+      const isScrollingUp = prevScrollPos > currentScrollPos;
+      setVisible(isScrollingUp);
+      setPrevScrollPos(currentScrollPos);
+    }
   };
-  const NotmenuVisibilty = () => {
-    setMenuBtn(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [prevScrollPos, menuClicked]);
+
+  const toggleMenuVisibility = () => {
+    setMenuBtn((prevMenuBtn) => !prevMenuBtn);
+    setMenuClicked(true);
+    setTimeout(() => setMenuClicked(false), 300); // Adjust the timeout duration as needed
   };
+
   return (
     <>
       <nav
-        className={`bg-white sm:flex sticky top-0 z-50 ${
+        className={`bg-white sm:flex sticky top-0 z-50 transition-transform duration-300 ${
           !menuBtn ? `justify-between p-3` : `justify-center`
-        } flex sm:p-6 content-center border-b-2`}
+        } flex sm:p-6 content-center border-b-2 ${
+          visible ? "translate-y-0" : "-translate-y-full"
+        }`}
       >
         <ul className="flex items-center justify-center">
-          {!menuBtn && <li className=" font-bold text-lg ">LOGO</li>}
+          {!menuBtn && <li className="font-bold text-lg">LOGO</li>}
         </ul>
 
-        <ul className=" space-x-8 text-lg hidden sm:flex">
+        <ul className="space-x-8 text-lg hidden sm:flex">
           <li>Home</li>
           <li>Recipe</li>
           <li>Blog</li>
@@ -39,32 +60,33 @@ export default function Nav() {
         </ul>
         <ul className="sm:flex space-x-5 text-md hidden justify-center items-center">
           <li>
-            {" "}
             <FaFacebookF />
           </li>
           <li>
-            {" "}
             <FaInstagram />
           </li>
           <li>
-            {" "}
             <FaTwitter />
           </li>
         </ul>
         {!menuBtn && (
           <button
-            onClick={menuVisibilty}
+            onClick={toggleMenuVisibility}
             className="flex sm:hidden text-lg p-1"
           >
             <CiMenuKebab />
           </button>
         )}
         {menuBtn && (
-          <div className=" bg-white flex flex-col h-72 w-full justify-between sticker sm:hidden">
-            <div className="flex justify-between items-center p-3  bg-white">
-              <div className=" font-bold text-lg">LOGO</div>{" "}
+          <div
+            className={`bg-white flex flex-col h-72 w-full justify-between sm:hidden transform transition-transform duration-300 ${
+              menuBtn ? "translate-y-0" : "-translate-y-full"
+            }`}
+          >
+            <div className="flex justify-between items-center p-3 bg-white">
+              <div className="font-bold text-lg">LOGO</div>
               <button
-                onClick={NotmenuVisibilty}
+                onClick={toggleMenuVisibility}
                 className="flex sm:hidden text-lg p-1"
               >
                 <IoCloseSharp />
@@ -72,7 +94,6 @@ export default function Nav() {
             </div>
 
             <ul className="flex justify-center items-center text-lg flex-col">
-              {" "}
               <li>Home</li>
               <li>Recipe</li>
               <li>Blog</li>
@@ -84,7 +105,6 @@ export default function Nav() {
                 <FaFacebookF />
               </li>
               <li>
-                {" "}
                 <FaInstagram />
               </li>
               <li>
@@ -95,13 +115,9 @@ export default function Nav() {
         )}
       </nav>
       <Recipe />
-      <Blog />
-
+      <Contact />
       <footer className="flex flex-col items-center md:flex-row border-t-2 md:px-4 xl:px-16">
-        <div
-          className="bg-white py-6 px-3 w-full items-center md:items-start flex flex-col flex-1
-        "
-        >
+        <div className="bg-white py-6 px-3 w-full items-center md:items-start flex flex-col flex-1">
           <p className=" text-lg font-bold md:text-2xl">LOGO</p>
           <p className="text-sm md:text-sm">
             {" "}
--- src/app/pages/recipe.tsx
+++ src/app/pages/recipe.tsx
@@ -7,6 +7,7 @@ import { BiDish } from "react-icons/bi";
 import { PiBowlFoodFill } from "react-icons/pi";
 import { IoTimer } from "react-icons/io5";
 import Email from "./components/email";
+import MoreRecipe from "./components/more-recipe";
 export default function recipe() {
   return (
     <>
@@ -174,101 +175,7 @@ export default function recipe() {
         <p className=" text-lg text-center font-bold md:text-2xl">
           You may like this recipe too
         </p>
-        <div className="grid grid-cols-2 md:grid-cols-3 gap-3 p-3 xl:grid-cols-4 xl:px-20">
-          {" "}
-          <div className=" flex flex-col gap-3 py-3 justify-between  rounded-2xl">
-            {" "}
-            <div className=" flex flex-col">
-              <img
-                src="https://veganwithgusto.com/wp-content/uploads/2022/03/PF-Chang-copycat-vegan-lettuce-wraps.jpg"
-                alt=""
-                className="object-cover h-36 md:h-48 xl:h-52 w-full rounded-2xl"
-              />
-            </div>
-            <p className="text-xs lg:text-lg font-bold xl:p-1">
-              Firecracker Vegan Lettuce Wraps-Spicy!
-            </p>
-            <div className="flex justify-between px-1 lg:text-2xl xl:p-1">
-              <div className="flex gap-1 items-center">
-                <IoTimer />
-                <p className="text-xs lg:text-sm">30m</p>
-              </div>
-              <div className="flex gap-1 items-center">
-                <PiBowlFoodFill />
-                <p className="text-xs">Healthy</p>
-              </div>
-            </div>
-          </div>
-          <div className=" flex flex-col gap-3 py-3 justify-between  rounded-2xl">
-            {" "}
-            <div className=" flex flex-col">
-              <img
-                src="https://www.allrecipes.com/thmb/_QlXcALXmJwnkIVRthr_6ux-XIg=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/176132-slow-cooker-buffalo-chicken-sandwiches-DDMFS-4x3-340-8946a0eef3a242e18746425348cd17dc.jpg"
-                alt=""
-                className="object-cover h-36 md:h-48 xl:h-52 w-full rounded-2xl"
-              />
-            </div>
-            <p className="text-xs lg:text-lg font-bold xl:p-1">
-              Barbeque Spicy Sandwitcs with Chips
-            </p>
-            <div className="flex justify-between px-1 lg:text-2xl xl:p-1">
-              <div className="flex gap-1 items-center">
-                <IoTimer />
-                <p className="text-xs lg:text-sm">30m</p>
-              </div>
-              <div className="flex gap-1 items-center">
-                <PiBowlFoodFill />
-                <p className="text-xs">Meat</p>
-              </div>
-            </div>
-          </div>
-          <div className=" flex flex-col gap-3 py-3 justify-between  rounded-2xl">
-            {" "}
-            <div className=" flex flex-col">
-              <img
-                src="https://i.pinimg.com/736x/1b/6f/cf/1b6fcf68843311ca79b1ef042627e34d.jpg"
-                alt=""
-                className="object-cover h-36 md:h-48 xl:h-52 w-full rounded-2xl"
-              />
-            </div>
-            <p className="text-xs lg:text-lg font-bold xl:p-1">
-              Rainbow Chiken Salad with Almond Honey Mustard Dressing al
-            </p>
-            <div className="flex justify-between px-1 lg:text-2xl xl:p-1">
-              <div className="flex gap-1 items-center">
-                <IoTimer />
-                <p className="text-xs lg:text-sm">30m</p>
-              </div>
-              <div className="flex gap-1 items-center">
-                <PiBowlFoodFill />
-                <p className="text-xs">Healthy</p>
-              </div>
-            </div>
-          </div>
-          <div className=" flex flex-col gap-3 py-3 justify-between  rounded-2xl">
-            {" "}
-            <div className=" flex flex-col">
-              <img
-                src="https://llbalanced.com/wp-content/uploads/2017/03/cauliflower-mushroom-taco-meat-3-1-of-1-scaled.jpg"
-                alt=""
-                className="object-cover h-36 md:h-48 xl:h-52 w-full rounded-2xl"
-              />
-            </div>
-            <p className="text-xs lg:text-lg font-bold xl:p-1">
-              Cauliflower Wallnut Vegetarian Taco Meal
-            </p>
-            <div className="flex justify-between px-1 lg:text-2xl xl:p-1">
-              <div className="flex gap-1 items-center">
-                <IoTimer />
-                <p className="text-xs lg:text-sm">30m</p>
-              </div>
-              <div className="flex gap-1 items-center">
-                <PiBowlFoodFill />
-                <p className="text-xs">Healthy</p>
-              </div>
-            </div>
-          </div>
-        </div>
+        <MoreRecipe />
       </div>
     </>
   );
